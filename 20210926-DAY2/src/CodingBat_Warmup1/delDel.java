2a70d653baed9ff52656741afe491c2bef62975c
KimsFamily <KimsFamily@DESKTOP-QMSTVJM>
2021-09-27 23:36:00 +0900

    20210926

---
+++ 20210926-DAY2/src/CodingBat_Warmup1/delDel.java
@@ -0,0 +1,20 @@
+package CodingBat_Warmup1;
+
+public class delDel {
+	public String delDel1(String str) {
+		  if(str.length()>3&&str.substring(1,4).equals("del"))
+		    return str.substring(0,1)+str.substring(4,str.length());
+		  else
+		    return str;
+	}
+	
+	// Solution
+	public String delDel2(String str) {
+		  if (str.length()>=4 && str.substring(1, 4).equals("del")) {
+		    // First char + rest of string starting at 4
+		    return str.substring(0, 1) + str.substring(4);
+		  }
+		  // Otherwise return the original string.
+		  return str;
+	}
+}
